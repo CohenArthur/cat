d985647d358f9cc2c77a0deba1b67ec285b6c3f9
yuuko-sama <yuuko@cock.li>
2015-04-25 20:01:07 -0700

    i don't even know but it's better now

--- cat.c
+++ cat.c
@@ -5,26 +5,26 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-void cat(const char *path)
+int cat(const char *path)
 {
 	FILE *fp = strcmp(path, "-") ? fopen(path, "r") : stdin;
 	char *buf;
 	struct stat fs;
-	if (fp) {
-		fstat(fileno(fp), &fs);
-	} else return;
-	if(posix_memalign((void **)&buf, sysconf(_SC_PAGESIZE), fs.st_blksize))
-		return;
-	if (isatty(fileno(fp))) {
+	if (!fp)
+		return 1;
+	fstat(fileno(fp), &fs);
+	if (posix_memalign((void **)&buf, sysconf(_SC_PAGESIZE), fs.st_blksize))
+		return 1;
+	if (errno = isatty(fileno(fp)) ? errno : 0) {
 		while (fgets(buf, (int)fs.st_blksize - 1, stdin))
 			fputs(buf, stdout);
 	} else while (!feof(fp)) 
 		fwrite(buf, 1, fread(buf, 1, fs.st_blksize, fp), stdout);
-	if (fp == stdin)
-		clearerr(fp);
+	if (fp == stdin) clearerr(fp);
 	else fclose(fp);
 		
 	free(buf);
+	return 0;
 }
 
 int main(int argc, char *argv[])
@@ -32,14 +32,12 @@ int main(int argc, char *argv[])
 	int i = 1, errors = 0;
 	
 	if (argc < 2)
-		cat("-");
-	else for (i = 1, errors = 0; i < argc; i++) {
-		cat(argv[i]);
-		if (errno) {
+		return cat("-");
+	while (i < argc) {
+		errors += cat(argv[i++]);
+		if (errno)
 			fprintf(stderr, "%s: %s: %s\n", 
 			        argv[0], argv[i], strerror(errno));
-			errors++;
-		}
 	}
 	return errors;
 }
